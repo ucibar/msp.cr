b8b43ed46be3d120ae9e22ebf362bfbb654b6158
Uğur Cibaroğlu <ugur.cibar@gmail.com>
2019-02-20 23:06:56 +0300

    attitude and raw_rc added.

---
+++ src/msp/get/attitude.cr
@@ -0,0 +1,20 @@
+module MSP::Get
+  struct Attitude
+    @@code = MSP::Code::ATTITUDE
+
+    getter angx : Int16,
+    angy : Int16,
+    heading : Int16
+
+    def initialize(@angx, @angy, @heading)
+    end
+
+    def self.from_slice(data : Bytes)
+      self.new(
+        MSP::ByteFormat.decode(Int16, data[0,2]),
+        MSP::ByteFormat.decode(Int16, data[2,2]),
+        MSP::ByteFormat.decode(Int16, data[4,2])
+      )
+    end
+  end
+end

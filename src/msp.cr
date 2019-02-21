7e92b88614d0b46c2c69cdd53297068bbc111f4b
Uğur Cibaroğlu <ugur.cibar@gmail.com>
2019-02-21 19:03:48 +0300

    comments changed.

--- src/msp.cr
+++ src/msp.cr
@@ -1,3 +1,4 @@
+require "json"
 require "./msp/*"
 require "./msp/get/*"
 require "./msp/set/*"
@@ -5,12 +6,12 @@ require "./msp/set/*"
 module MSP
   VERSION = "0.1.0"
   # The general format of an MSP message is:
-  # <preamble>,<direction>,<data-size>,<command>,<data>,<crc>
+  # <preamble>,<direction>,<data-size>,<msp-code>,<data>,<checksum>
   PREAMBLE          = "$M"                 # MSP Message Preamble
   TO_MWC            = '<'                  # MSP Message To Multiwii Controller
   FROM_MWC          = '>'                  # MSP Message From Multiwii Controller
-  SIZE_BYTE_INDEX   = 3                    # Byte Index Of Size Byte
-  DATA_BEGIN_INDEX  = 5                    # Byte Index Of Data Begin
+  SIZE_BYTE_INDEX   = 3                    # Byte Index Of 'data-size' Byte
+  DATA_BEGIN_INDEX  = 5                    # Byte Index Of 'data' Begin
   SIZE_WITHOUT_DATA = DATA_BEGIN_INDEX + 1 # MSP Message Byte Size Without Data Bytes, '+ 1' Is Checksum Byte
 
   alias ByteFormat = IO::ByteFormat::LittleEndian
